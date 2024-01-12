c11c22471e911f5cef908cc4acc63c158f76c2dc
Brandon <brandonbgrayson@gmail.com>
2024-01-11 21:37:33 -0500

    login user route with base user model schema, returns usercredentials

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+class User(BaseModel):
+    email: str
+    password: str
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000"
+];
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/login")
+async def create_user(user_credentials: User):
+    print(user_credentials)
+
+    return user_credentials
