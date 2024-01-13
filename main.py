38034b64c63308fa684fe061b520ae334b627a8c
Brandon <brandonbgrayson@gmail.com>
2024-01-13 15:05:54 -0500

    password hashing functions added

--- main.py
+++ main.py
@@ -1,13 +1,47 @@
-from fastapi import FastAPI
+from datetime import datetime, timedelta, timezone
+from typing import Annotated, Union
+
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
+from passlib.context import CryptContext
 from pydantic import BaseModel
-from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
+SECRET_KEY = "88b4fe011dd15250dbb5fecb8c5725c03b53674147f2bb257534aa618b3e4065"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    email: Union[str, None] = None
 
 class User(BaseModel):
     email: str
     password: str
 
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
 app = FastAPI()
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+# def authenticate_user(fake_db, username: str, password: str):
+#     user = get_user(fake_db, username)
+#     if not user:
+#         return False
+#     if not verify_password(password, user.hashed_password):
+#         return False
+#     return user
 
 origins = [
     "http://localhost:3000"
